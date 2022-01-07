b0d715e0e3ee1a3b74e63f0e0bb25bf6f93010da
santiago <truefush19@gmail.com>
2022-01-07 17:56:17 -0500

    UPDATE

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let menu = document.querySelector('.menu');
 let navbar = document.querySelector('.menu_deb');
 
-menu.onclick = () =>{
+menu.addEventListener("click", () => {
     navbar.classList.toggle('active');
     menu.classList.toggle('move');
-}
+});
 
+//notification
+
+let bell = document.querySelector('.notification');
+
+document.querySelector('#bell').addEventListener("click",() => {
+    bell.classList.toggle('active');
+});
