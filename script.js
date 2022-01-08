2d789799fd38c7140413a128bcc68850663f11b0
santiago <truefush19@gmail.com>
2022-01-07 19:19:06 -0500

    [UPDATE]

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let navbar = document.querySelector('.menu_deb');
 menu.addEventListener("click", () => {
     navbar.classList.toggle('active');
     menu.classList.toggle('move');
+    bell.classList.remove('active');
 });
 
 //notification
-
 let bell = document.querySelector('.notification');
 
 document.querySelector('#bell').addEventListener("click",() => {
